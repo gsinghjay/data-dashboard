979cba5937f208e96373c56c1ac210ee4add69dd
Jay Singh <54427246+gsinghjay@users.noreply.github.com>
2025-05-11 17:25:13 -0400

    feat: export FertilityPandemicChart in charts index

--- src/components/charts/index.ts
+++ src/components/charts/index.ts
@@ -1,4 +1,5 @@
 export { default as FertilityBarChart } from './FertilityBarChart';
 export { default as FertilityLineChart } from './FertilityLineChart';
 export { default as FertilityMapChart } from './FertilityMapChart';
-export { default as FertilityMilestonesChart } from './FertilityMilestonesChart'; 
+export { default as FertilityMilestonesChart } from './FertilityMilestonesChart';
+export { default as FertilityPandemicChart } from './FertilityPandemicChart'; 
